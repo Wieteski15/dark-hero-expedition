2a4dd4e3322d8264f5c23de427522ba0a8447891
Wieteski <58888738+Wieteski15@users.noreply.github.com>
2026-01-20 18:24:19 +0100

    first auto-battle works

--- FileSystem/data/character/Character.gd
+++ FileSystem/data/character/Character.gd
@@ -6,14 +6,17 @@ class_name Character
 @export var max_hp: int = 100
 @export var hp: int = 100
 
-@export var attak: int = 10
+@export var attack: int = 10
 @export var speed: float = 1.0
 
 @export var morale: int = 100
+@export var max_morale: int = 100
 
-@export var is_live: bool = true
+@export var is_alive: bool = true
 
 @export var pernament_wonds: Array[String] = []
 @export var equipment: Array = []
 
 @export var death_cause: String = ""
+
+@export var action_timer: float = 0.0
